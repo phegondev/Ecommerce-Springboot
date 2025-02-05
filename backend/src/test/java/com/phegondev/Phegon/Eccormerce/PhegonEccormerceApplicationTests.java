5586fb14d030c191da183c8c026c10f37653ae33
Scavenger233 <liyiminginireland@gmail.com>
2025-02-05 11:17:46 +0000

    delete secrete msg

--- backend/src/test/java/com/phegondev/Phegon/Eccormerce/PhegonEccormerceApplicationTests.java
+++ backend/src/test/java/com/phegondev/Phegon/Eccormerce/PhegonEccormerceApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+<<<<<<< HEAD
 class PhegonEccormerceApplicationTests {
 
 	@Test
@@ -11,3 +12,19 @@ class PhegonEccormerceApplicationTests {
 	}
 
 }
+=======
+class PhegonEccormerceApplicationTest {
+
+	@Test
+	public void contextLoads() {
+		// 检查 Spring Boot 应用程序是否能够正常启动
+	}
+
+	@Test
+	public void main() {
+		// 测试 main 方法，确保没有抛出异常
+		String[] args = {};
+		PhegonEccormerceApplication.main(args);
+	}
+}
+>>>>>>> 851b1e8 (delete secrete msg)
